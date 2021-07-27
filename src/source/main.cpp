3d4e32a06c368abc18afee71d5c00078fc651fee
k4-100 <k4-100@protonmail.com>
2021-07-27 16:49:38 +0200

    Refactored main.cpp and filled it with content 1. window is now a fixed size 800x800 square 2. window.pollEvent( event )'s while loop uses switch now

--- src/source/main.cpp
+++ src/source/main.cpp
@@ -1,22 +1,36 @@
+// external libs
 #include <SFML/Graphics.hpp>
+
+// core c/c++
+#include <iostream> 
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window
+    ( 
+        sf::VideoMode( 800, 800 ), 
+        "SFML works!", 
+        sf::Style::Close 
+    );
 
-    while (window.isOpen())
+    sf::CircleShape shape( 100.f );
+    shape.setFillColor( sf::Color::Green );
+
+    while ( window.isOpen() )
     {
         sf::Event event;
-        while (window.pollEvent(event))
+        while ( window.pollEvent( event ) )
         {
-            if (event.type == sf::Event::Closed)
-                window.close();
+            switch( event.type ){
+                case sf::Event::Closed:
+                    window.close();
+                break; 
+            }
+                
         }
 
         window.clear();
-        window.draw(shape);
+        window.draw( shape );
         window.display();
     }
 
